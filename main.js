3c7862fb9fcf2cb3100f767bba46f309612da54f
Benjamin White <32458896+BenWhite89@users.noreply.github.com>
2017-10-06 12:46:37 -0500

    Make it work

--- main.js
+++ main.js
@@ -2,30 +2,78 @@ $(function() {
 
   $("#keyboard-upper-container").hide();
 
+  
+
   var sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'],
       sentenceNumber = 0,
       letterNumber = 0,
       checkString = '',
-      highlight = $("#yellow-block").offset().left;
-     
+      highlightOriginal = $("#yellow-block").offset().left,
+      highlight = highlightOriginal,
+      checkCount = 0,
+      xCount = 0,
+      timerStart = Date.now(),
+      wordCount = 54;
   
-  $("#sentence").prepend("<p id='p-sentence'>" + sentences[sentenceNumber] + "</p>");
-  $("#feedback").prepend("<p id='p-feedback'></p>"); 
-  $("#target-letter").prepend("<p id='p-target'></p>");
-  
-  function displayTarget(s, l) {
-    if (checkString != sentences[sentenceNumber]) {
-      $("#p-target").html(sentences[s].charAt(l));
-      console.log(sentences[sentenceNumber]);
 
-    } else if (checkString == sentences[sentenceNumber]){
-      sentenceNumber = sentenceNumber + 1;
-      $("#p-sentence").empty();
-      $("#p-sentence").text(sentences[sentenceNumber]);
+  $("#sentence").prepend("<p id='p-sentence'></p>");
+  $("#target-letter").prepend("<p id='p-target'></p>");
+  $("#p-sentence").text(sentences[sentenceNumber]);
+
+  function displayTarget(s, l) {
+      $("#p-target").text(sentences[s].charAt(l));
+  }
+
+
+  function displaySentence() {
+      if (checkString == sentences[sentenceNumber]){
+        sentenceNumber = sentenceNumber + 1;
+        if(sentenceNumber != 5) {
+          $("#p-sentence").empty();
+          $("#p-sentence").text(sentences[sentenceNumber]);
+          $("#feedback").empty();
+          letterNumber = 0;
+          displayTarget(sentenceNumber, letterNumber);
+          checkString = '';
+        } else {
+          var timerEnd = Date.now(),
+              duration = (timerEnd - timerStart) / 60000,
+              rate = Math.round(wordCount/duration),
+              factor = 2 * xCount,
+              weightedRate = rate - factor;
+             
+          if(window.confirm(`You type at ${weightedRate} words per minute! Try again? `)) {
+           
+              sentenceNumber = 0;
+              letterNumber = 0;
+              checkString = '';
+              highlight = highlightOriginal;
+              checkCount = 0;
+              xCount = 0;
+              timerStart = Date.now();
+              $("#p-sentence").empty();
+              $("#p-sentence").text(sentences[sentenceNumber]);
+              $("#feedback").empty();
+              displayTarget(sentenceNumber, letterNumber);
+            
+          };
+        };
+      };
+  }
+
+  function highlightPlacement() {
+    if(checkString != sentences[sentenceNumber]){
+      highlight = highlight + 17;
+      $("#yellow-block").css('left', highlight);
+    } else if (checkString == sentences[sentenceNumber]) {
+      highlight = highlightOriginal;
+      $("#yellow-block").css('left', highlight);
     }
   }
 
-  displayTarget(letterNumber);
+  
+
+  displayTarget(sentenceNumber, letterNumber);
   
   $('body').on('keydown', function(e) {
     if (e.which == 16) {
@@ -47,6 +95,7 @@ $(function() {
   $('body').on('keypress', function(e) {
     var keyPress = e.which,
         keyString = String.fromCharCode(keyPress);
+        
        
       $("#" + keyPress).addClass("press");
       
@@ -55,12 +104,15 @@ $(function() {
          letterNumber = letterNumber + 1;
          displayTarget(sentenceNumber, letterNumber);
          checkString = checkString + keyString;
-         console.log(checkString);
-         highlight = highlight + 16;
-         $("#yellow-block").css('left', highlight);
-        
-         
+         checkCount = checkCount + 1;
+         $("#feedback").append("<span class='glyphicon glyphicon-ok' id='" + checkCount + "'></span>");
+         highlightPlacement();
+         displaySentence();
 
+      } else {
+        xCount = xCount + 1;
+        $("#feedback").append("<span class='glyphicon glyphicon-remove' id='" + xCount + "'></span>");
+        
       };
       
 
